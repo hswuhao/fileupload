59effe5e1946ca051fef895089219b3c4e7d184f
Sezer Tunca <sezertunca@gmail.com>
2016-03-23 11:43:12 +0000

    Update file upload limit size

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -14,7 +14,7 @@ class UploadController extends Controller
     {
     	$this->validate($request, 
     	[
-    		'file' => 'required|mimes:zip'
+    		'file' => 'required|max:100000|mimes:zip'
     	]);
     	$file = $request->file('file');
 
