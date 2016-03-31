1fb488a72d7ea4ddd7167dfad2b6edc25cd05288
Sezer Tunca <sezertunca@gmail.com>
2016-03-31 18:38:36 +0100

    Add max_file_size field to upload HTML form

--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -5,6 +5,7 @@
         
         <form id="fileUploadForm" action="/upload" class="dropzone">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="MAX_FILE_SIZE" value="90000">
                   <div class="fallback">
                     <input name="file" type="file" multiple />
                   </div>
