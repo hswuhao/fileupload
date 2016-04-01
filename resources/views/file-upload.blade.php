627c5d41ae2cb9f1c10907af43924a8778ba36a8
Sezer Tunca <sezertunca@gmail.com>
2016-04-01 14:30:56 +0100

    Add enctype to upload html form

--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -1,15 +1,10 @@
 @extends('master')
 
-
-@section('content')
-        
-        <form id="fileUploadForm" action="/upload" class="dropzone">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="hidden" name="MAX_FILE_SIZE" value="90000">
-                  <div class="fallback">
-                    <input name="file" type="file" multiple />
-                  </div>
-                </form>
-            </div>
-    
+@section('content')    
+    <form id="fileUploadForm" action="/upload" class="dropzone" enctype="multipart/form-data">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="fallback">
+       		<input name="file" type="file" multiple />
+        </div>
+    </form>
 @endsection
